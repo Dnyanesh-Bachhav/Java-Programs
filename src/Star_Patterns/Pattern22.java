883f972e99dda9a92cb9581977c8174f2c1433bd
Dnyanesh Bachhav <dnyaneshbachhav2002@gmail.com>
2025-02-26 18:15:08 +0530

    Patterns added...

--- src/Star_Patterns/Pattern22.java
+++ src/Star_Patterns/Pattern22.java
@@ -1,4 +1,24 @@
 package Star_Patterns;
 
 public class Pattern22 {
+    public static void main(String args[])
+    {
+        int ROWS = 5;
+        int COLS = 5;
+        int N = 4;
+        for(int i=0;i<2*N-1;i++)
+        {
+            for(int j=0;j<2*N-1;j++)
+            {
+                int top = i;
+                int bottom = j;
+                int right = (2*N-2)-j;
+                int left = (2*N-2)-i;
+
+                int num = N - Math.min(Math.min(top, bottom), Math.min(left, right));
+                System.out.print(num);
+            }
+            System.out.println();
+        }
+    }
 }
